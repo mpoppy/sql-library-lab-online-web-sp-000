2613508381fb13f1cbe50a9a328010cda867d257
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 01:38:55 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,14 +2,14 @@
 CREATE TABLE authors (
   id INTEGER  PRIMARY KEY,
   name TEXT
-)
+);
 
 CREATE TABLE books (
   id INTEGER  PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series_id INTEGER
-)
+);
 
 CREATE TABLE characters (
   id INTEGER  PRIMARY KEY,
@@ -17,22 +17,22 @@ CREATE TABLE characters (
   motto TEXT,
   species TEXT,
   author_id INTEGER
-)
+);
 
 CREATE TABLE series (
   id INTEGER  PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
   subgenre_id INTEGER
-)
+);
 
 CREATE TABLE subgenres (
   id INTEGER  PRIMARY KEY,
   name TEXT
-)
+);
 
 CREATE TABLE character_books (
   id INTEGER  PRIMARY KEY,
   book_id INTEGER,
   character_id INTEGER
-)
+);
