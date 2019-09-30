5999c6e10dc88390252c1ff67702033c97cbbfdb
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 01:38:09 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -31,8 +31,10 @@ CREATE TABLE subgenres (
   name TEXT
 )
 
-CREATE TABLE authors (
-  id INTEGER  PRIMARY KEY
+CREATE TABLE character_books (
+  id INTEGER  PRIMARY KEY,
+  book_id INTEGER,
+  character_id INTEGER
 )
 
 CREATE TABLE authors (
