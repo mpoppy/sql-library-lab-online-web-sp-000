6c4d939a865ffdd3091f2b1304ddbe0fd4d0625b
Marc Poppy <poppy.marc@gmail.com>
2019-09-30 03:03:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,11 +30,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, count(characters.species)
+  "SELECT series.title
   FROM series
   INNER JOIN authors ON authors.id = series.author_id
   INNER JOIN characters ON authors.id = characters.author_id
-  WHERE characters.species = 'human';"
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY count(characters.species) DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
